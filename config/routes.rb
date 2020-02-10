bee922d6ebf86b9f69a9eee4435ec4b559b49e05
Yann-Leynaert <yann.leynaert@hotmail.fr>
2020-02-10 19:50:07 +0100

    appli

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  get '/team', to: 'static#team'
+  get '/contact', to: 'static#contact'
+   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
